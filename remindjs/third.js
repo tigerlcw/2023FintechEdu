29c87835274d19bb354b9fa9ab0fefce6eedc15b
LINKER <temp2temp@daum.net>
2023-08-01 11:48:13 +0900

    lecture: Arrow Funtion

---
+++ remindjs/third.js
@@ -0,0 +1,20 @@
+const plus = (p1, p2) => {
+  console.log(p1 + p2);
+};
+
+const minus = (p1, p2) => {
+  console.log(p1 - p2);
+};
+
+const multiply = (p1, p2) => {
+  console.log(p1 * p2);
+};
+
+const divide = (p1, p2) => {
+  console.log(p1 / p2);
+};
+
+plus(1, 2);
+minus(1, 2);
+multiply(1, 2);
+divide(1, 2);
