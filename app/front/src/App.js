a0f4ca6b3774b5bc240a7ac14a9bedda0e90ce21
LINKER <temp2temp@daum.net>
2023-08-01 13:52:07 +0900

    lecture react 컴포넌트 생성 및 사용

--- app/front/src/App.js
+++ app/front/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+//컴포넌트 생성
+const Welcome = () => {
+  return <h1>Hello World</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome /> {/* 컴포넌트 사용 */}
     </div>
   );
 }
