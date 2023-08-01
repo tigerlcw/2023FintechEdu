25336b80234c4cc84bcf90565b5bdd1ec48742cc
LINKER <temp2temp@daum.net>
2023-08-01 14:14:14 +0900

    lecture react props

--- app/front/src/App.js
+++ app/front/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 
 //컴포넌트 생성
-const Welcome = () => {
-  return <h1>Hello World</h1>;
+const Welcome = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>안녕하세요! {props.username} 님.</h1>
+      <h2>{props.age}세 이시군요</h2>
+    </div>
+  );
 };
 
 function App() {
   return (
     <div className="App">
-      <Welcome /> {/* 컴포넌트 사용 */}
+      <Welcome username="이누구" age={14} /> {/* 컴포넌트 사용 */}
+      <Welcome username="김누구" age={20} />
     </div>
   );
 }
