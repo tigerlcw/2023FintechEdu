f1564fc0f4d53635557d71fff7819fa0f6288cef
LINKER <temp2temp@daum.net>
2023-08-02 15:26:34 +0900

    feat: 계좌 목록 조회 기능 1

--- app/front/src/pages/AuthResultPage.js
+++ app/front/src/pages/AuthResultPage.js
@@ -10,6 +10,8 @@ const AuthResultPage = () => {
   const parsed = queryString.parse(queryParams);
   console.log(parsed.code);
   const authCode = parsed.code;
+  const [accessToken, setAccessToken] = useState('');
+  const [userSeqNo, setUserSeqNo] = useState('');
 
   const handleClick = () => {
     //axios 요청 만들기
@@ -30,9 +32,15 @@ const AuthResultPage = () => {
     };
     axios(requestOption).then(({ data }) => {
       // localStorage에 저장 -> 실무에서는 로컬스토리지보다는 쿠키에 저장
-      const { access_token, user_seq_no } = data;
-      localStorage.setItem('accessToken', access_token);
-      localStorage.setItem('userSeqNo', user_seq_no);
+      setAccessToken(data.access_token);
+      setUserSeqNo(data.user_seq_no);
+      if (data.rsp_code !== 'O0001') {
+        localStorage.setItem('accessToken', data.access_token);
+        localStorage.setItem('userSeqNo', data.user_seq_no);
+        alert('저장 완료');
+      } else {
+        alert('인증에 실패했습니다 다시 시도해 주세요');
+      }
     });
   };
 
@@ -41,8 +49,8 @@ const AuthResultPage = () => {
       <AppHeader title={'인증결과 / 토큰 생성'}></AppHeader>
       <p>코드 : {authCode}</p>
       <button onClick={handleClick}>accessToken 요청</button>
-      <p>accessToken : {data.access_token}</p>
-      <p>userSeqNo : {user_seq_no}</p>
+      <p>accessToken : {accessToken}</p>
+      <p>userSeqNo : {userSeqNo}</p>
     </div>
   );
 };
