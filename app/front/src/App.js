c44cfb72bb003a693643981993e9da68bb5f11f6
LINKER <temp2temp@daum.net>
2023-08-03 11:40:41 +0900

    feat: 결제 QR code 생성

--- app/front/src/App.js
+++ app/front/src/App.js
@@ -4,6 +4,7 @@ import AuthPage from './pages/AuthPage';
 import AuthResultPage from './pages/AuthResultPage';
 import MainPage from './pages/MainPage';
 import BalancePage from './pages/BalancePage';
+import QrCodePage from './pages/QrCodePage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/main" element={<MainPage />}></Route>
         <Route path="/authResult" element={<AuthResultPage />}></Route>
         <Route path="/balance" element={<BalancePage />}></Route>
+        <Route path="/qrcode" element={<QrCodePage />}></Route>
         <Route path="/news" element={<NewsPage />}></Route>
       </Routes>
     </BrowserRouter>
