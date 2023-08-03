13ebbe62bef1753e8bce4d5c2b904acb2e3074be
LINKER <temp2temp@daum.net>
2023-08-03 15:49:09 +0900

    feat: 출금 기능 구현 2

--- app/front/src/Components/withdraw/ModalCard.js
+++ app/front/src/Components/withdraw/ModalCard.js
@@ -30,7 +30,6 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   //fintechUseNo : 내계좌
   //tofintechno : QR 코드로 읽어온 핀테크 계좌
   const [amount, setamount] = useState('');
-  let accessToken = localStorage.getItem('accessToken');
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
@@ -46,6 +45,9 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
     // axios option으로 요청을 작성하기 <- api 요청
     // application/json 은 데이터를 어떻게 전송?
     // 결과를 로그로 작성
+
+    const accessToken = localStorage.getItem('accessToken');
+
     const data = {
       bank_tran_id: genTransId(),
       cntr_account_type: 'N',
@@ -54,16 +56,33 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
       fintech_use_num: fintechUseNo,
       wd_print_content: '오픈뱅킹출금',
       tran_amt: amount,
-      tran_dtime: '20230317130000',
+      tran_dtime: '20230802130000',
       req_client_name: '홍길동',
       req_client_fintech_use_num: fintechUseNo,
       req_client_num: 'HONGGILDONG1234',
       transfer_purpose: 'ST',
-      recv_client_name: '유관우',
+      recv_client_name: '홍길동',
       recv_client_bank_code: '097',
       recv_client_account_num: '100000000001',
     };
+
     console.log(data);
+    // tran_amt, fintech_use_num, req_client_fintech_use_num, bank_tran_id 고정값 사용 금지 나머지는 고정값으로
+    // axios option으로 요청을 작성하기 <- api 요청
+    const option = {
+      method: 'POST',
+      url: '/v2.0/transfer/withdraw/fin_num',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data,
+    };
+    axios(option).then(({ data }) => {
+      console.log(data);
+      if (data.rsp_code === 'A0000') {
+        alert('출금 완료');
+      }
+    });
   };
 
   const deposit = () => {
@@ -73,46 +92,23 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
      * 2legged token 사용 !
      * 입금을 하는 계좌를 잘 선택해 주세요
      */
-    const data = {
-      cntr_account_type: 'N',
-      cntr_account_num: '200000000001',
-      wd_pass_phrase: 'NONE',
-      wd_print_content: '환불금액',
-      name_check_option: 'off',
-      tran_dtime: '20220812130000',
-      req_cnt: '1',
-      req_list: [
-        {
-          tran_no: '1',
-          bank_tran_id: genTransId(),
-          fintech_use_num: tofintechno,
-          print_content: '쇼핑몰환불',
-          tran_amt: amount,
-          req_client_name: '홍길동',
-          req_client_fintech_use_num: fintechUseNo,
-          req_client_num: '1234',
-          transfer_purpose: 'ST',
-        },
-      ],
-    };
-    console.log(data);
-
-    const handleChange = (e) => {
-      const { value } = e.target;
-      console.log(value);
-      setamount(value);
-    };
-
-    return (
-      <ModalCardBlock>
-        <CardTitle>{bankName}</CardTitle>
-        <FintechUseNo>{fintechUseNo}</FintechUseNo>
-        <p>{tofintechno}로 돈을 보냅니다.</p>
-        <input onChange={handleChange}></input>
-        <WithDrawButton onClick={handlePayButtonClick}>결제하기</WithDrawButton>
-      </ModalCardBlock>
-    );
   };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setamount(value);
+  };
+
+  return (
+    <ModalCardBlock>
+      <CardTitle>{bankName}</CardTitle>
+      <FintechUseNo>{fintechUseNo}</FintechUseNo>
+      <p>{tofintechno}로 돈을 보냅니다.</p>
+      <input onChange={handleChange}></input>
+      <WithDrawButton onClick={handlePayButtonClick}>결제하기</WithDrawButton>
+    </ModalCardBlock>
+  );
 };
 
 export default ModalCard;
