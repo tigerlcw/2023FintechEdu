777f714eb8e8567b67e93cd61538da8ebb6d3912
LINKER <temp2temp@daum.net>
2023-08-03 14:14:43 +0900

    feat: QR Reader 기능 구현

--- app/front/src/App.js
+++ app/front/src/App.js
@@ -5,6 +5,7 @@ import AuthResultPage from './pages/AuthResultPage';
 import MainPage from './pages/MainPage';
 import BalancePage from './pages/BalancePage';
 import QrCodePage from './pages/QrCodePage';
+import QrReaderPage from './pages/QrReaderPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/authResult" element={<AuthResultPage />}></Route>
         <Route path="/balance" element={<BalancePage />}></Route>
         <Route path="/qrcode" element={<QrCodePage />}></Route>
+        <Route path="/qrreader" element={<QrReaderPage />}></Route>
         <Route path="/news" element={<NewsPage />}></Route>
       </Routes>
     </BrowserRouter>
