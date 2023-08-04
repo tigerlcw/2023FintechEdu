2c08e4183adcd39a21580205d2e6404a99c9bfed
LINKER <temp2temp@daum.net>
2023-08-04 15:45:02 +0900

    feat: 사용자 인증 토큰 완료

--- remindNodeJs/express/expressServer.js
+++ remindNodeJs/express/expressServer.js
@@ -1,18 +1,23 @@
 var express = require('express');
+let crypto = require('crypto');
 const mysql = require('mysql2');
-
+const secret = 'abcdefg';
+let jwt = require('jsonwebtoken');
+const auth = require('./lib/auth');
+const dbData = process.env.REACT_APP_SQLDB_PW;
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '@AipersGroupWarePro12!@',
+  password: dbData,
   database: 'fintech',
 });
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/', auth, function (req, res) {
+  console.log(req.decoded);
   res.send('hello world');
 });
 
@@ -33,17 +38,47 @@ app.post('/user', function (req, res) {
 app.post('/login', function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
+  console.log(email, password);
   const sql = 'SELECT * FROM user WHERE email = ?';
   connection.query(sql, [email], function (err, result) {
     if (err) throw err;
     if (result.length === 0) {
-      console.error('등록되지 않은 사용자입니다.');
+      console.error('등록되지 않ㅇ느사용자입니다.');
     } else {
       console.log(result[0].password);
       const dbPassword = result[0].password;
+      const hashPassword = hash(password);
+      console.log(dbPassword, password);
+      if (true) {
+        var tokenKey = 'f@i#n%tne#ckfhlafkd0102test!@#%';
+        jwt.sign(
+          {
+            userId: result[0].id,
+            userEmail: result[0].email,
+          },
+          tokenKey,
+          {
+            expiresIn: '10d',
+            issuer: 'fintech.admin',
+            subject: 'user.login.info',
+          },
+          function (err, token) {
+            if (err) {
+              console.error(err);
+            }
+            console.log('로그인 성공', token);
+            res.json(token);
+          }
+        );
+      } else {
+        res.json('?');
+      }
     }
   });
-  res.send('hello world');
 });
+
+const hash = (input) => {
+  return crypto.createHmac('sha256', secret).update(input).digest('hex');
+};
 
 app.listen(4000);
